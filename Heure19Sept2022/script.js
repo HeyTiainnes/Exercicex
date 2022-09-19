2aa050312b9406aabae60a3fbdf1a18487fd8b83
HeyTiainnes <etienne.pomente@reseau.sncf.fr>
2022-09-19 19:55:53 +0200

    message

--- Heure19Sept2022/script.js
+++ Heure19Sept2022/script.js
@@ -1,4 +1,54 @@
 // recuperer heure //
+    // document.querySelector('p')
 // trouver generer heure actuelle
 // recuperer heure dans format HH:MM:SS (methode)
-// Modifier le contenu de notre paragraphe (afficher heure)
+// Modifier le contenu 
+
+//////////////////////
+/////CORRECTION////////
+///////////////////////
+
+
+
+
+ let paragraphe = document.querySelector('p');
+    
+    //on recupere le premier paragraphe et on le stock ds variaible
+console.log(paragraphe);
+let curentDate = new Date ();
+
+let heures = curentDate.getHours();
+let minutes = curentDate.getMinutes();
+let secondes = curentDate.getSeconds();
+console.log("heure", heures);
+console.log("minutes", minutes);
+console.log("secondes", secondes);
+
+let affichageHeure = heures +':' + minutes+ ':'+ secondes;
+
+//let affichageHeure = '${heures}:${minutes}:${secondes}';
+console.log(paragraphe.innerHTML);
+
+paragraphe.innerHTML(affichageHeure);
+
+
+
+//inner text pour renvoyer texte
+/*let current = new Date();
+let monHeure = new Object
+/*({
+    heures : getHours(),
+    minutes : getMinutes(),
+    secondes : getSecondes()
+
+});*/
+/*
+console.log(paragraphe);
+new Object {
+
+heure : getHours();
+minutes : getMinutes();
+secondes : getSeconds();
+
+}
+*/
--- script.js
+++ script.js
@@ -1,8 +1,51 @@
 // recuperer heure //
+    // document.querySelector('p')
 // trouver generer heure actuelle
 // recuperer heure dans format HH:MM:SS (methode)
 // Modifier le contenu 
 
+//////////////////////
+/////CORRECTION////////
+///////////////////////
 
 
-document.write(Date.prototype.getHours();
+
+
+ let paragraphe = document.querySelector('p')
+    
+    //on recupere le premier paragraphe et on le stock ds variaible
+console.log(paragraphe);
+let curentDate = new Date ();
+
+let heures = curentDate.getHours();
+let minutes = curentDate.getMinutes();
+let secondes = curentDate.getSeconds();
+console.log("heure", heures);
+console.log("minutes", minutes);
+console.log("secondes", secondes);
+
+document.open();
+let affichageHeure = 
+document.write(paragraphe);
+document.close();
+
+
+
+/*let current = new Date();
+let monHeure = new Object
+/*({
+    heures : getHours(),
+    minutes : getMinutes(),
+    secondes : getSecondes()
+
+});*/
+/*
+console.log(paragraphe);
+new Object {
+
+heure : getHours();
+minutes : getMinutes();
+secondes : getSeconds();
+
+}
+*/
