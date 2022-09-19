0d80e019e93d9839ac96030e428addf2c4b61ad4
Tiainaittes <etienne.pomente@reseau.sncf.fr>
2022-09-19 19:51:29 +0200

    message

---
+++ Heure19Sept2022/script.js
@@ -0,0 +1,4 @@
+// recuperer heure //
+// trouver generer heure actuelle
+// recuperer heure dans format HH:MM:SS (methode)
+// Modifier le contenu de notre paragraphe (afficher heure)
---
+++ script.js
@@ -0,0 +1,8 @@
+// recuperer heure //
+// trouver generer heure actuelle
+// recuperer heure dans format HH:MM:SS (methode)
+// Modifier le contenu 
+
+
+
+document.write(Date.prototype.getHours();
